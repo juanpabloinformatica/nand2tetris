1d7db64ace02f2d96bdf3e589f0e620865d66b0e
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2024-11-25 18:19:55 +0100

    Still missing clear the screen

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -3,9 +3,44 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+(LOOP1)
+  // location of the keyboard or RAM[24576]
+  @24576
+  D=M
+  @FALSE
+  D;JEQ
+  @0
+  D=A
+  @i
+  M=D
+  // area of the screen
+  @8192
+  D=A
+  @n
+  M=D
+  (LOOP2)
+  @n
+  D=M
+  @i
+  D=D-M
+  @FALSE
+  D;JLE
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  M=-1
+  @i
+  M=M+1
+  @LOOP2
+  0;JMP
+(FALSE)
+  @LOOP1
+  0;JMP
+
